c5c0b0b25445f3f63ad8a2236f6241ae18254b99
MarcosARGomes <marcosandrebahia@gmail.com>
2023-09-27 22:47:33 -0300

    Menu responsivo
    Construção do menu responsivo e função de abrir e fechar.

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "../../assets/Logo.png";
 import Link from './Link';
 import { SelectedPage } from '@/share/types';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import { ActionButton } from '@/share/ActionButton';
 
 type Props = {
     selectedPage: SelectedPage;
@@ -52,12 +53,12 @@ const Navbar = ({
                             </div>
                             <div className={`${flexBetween} gap-8`}>
                                 <p>Sign In</p>
-                                <button>Become a Member</button>
+                                <ActionButton setSelectedPage={setSelectedPage} >Become a Member</ActionButton>
                             </div>
                         </div>)
                             : (<button className='rounded-full bg-secondary-500 p-2'
-                            onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                            <Bars3Icon className='h-6 w-6 text-white'/>
+                                onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                                <Bars3Icon className='h-6 w-6 text-white' />
 
                             </button>
 
@@ -65,6 +66,41 @@ const Navbar = ({
                     </div>
                 </div>
             </div>
+
+            {/* Mobile menu modal */}
+            {!isAboveMediumScreens && isMenuToggled && (
+                <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
+                    {/*Close Icon */}
+                    <div className='flex justify-end p-12'>
+                        <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <XMarkIcon className='h-6 w-6 text-gray-400' />
+                        </button>
+                    </div>
+                    {/*Menu Items */}
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                                <Link
+                                    page="Home"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link
+                                    page="Benefits"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link
+                                    page="Our Classes"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                                <Link
+                                    page="Contact Us"
+                                    selectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
+                                />
+                            </div>
+                </div>
+            )}
         </nav>
     )
 }
