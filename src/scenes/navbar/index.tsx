63d34d77b97c78b49de2d8f1ebccc5be9278d5c9
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2023-10-04 09:48:26 -0400

    Alteração da logo

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
-import Logo from "../../assets/Logo.png";
+import Logo from "../../assets/Logo2.png";
+
 import Link from './Link';
 import { SelectedPage } from '@/share/types';
 import useMediaQuery from '@/hooks/useMediaQuery';
