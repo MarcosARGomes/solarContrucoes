27d9ee33b5a22e95cc62dc37c6a1745044574946
MarcosARGomes <marcosandrebahia@gmail.com>
2023-10-07 16:30:55 -0300

    Inclusão de contatos.

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,26 +1,31 @@
 import Logo from "@/assets/Solar_Construções.png";
+import Instagram from "@/assets/instagran.png";
+import WhatsApp from "@/assets/whatsapp.png";
+
+
 
 type Props = {}
 
 const Footer = (props: Props) => {
     return (
-        <footer className="bg-primary-100 py-16">
-            <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <footer className="bg-primary-100 py-5">
+            <div className="justify-content mx-auto w-5/6 gap-10 md:flex flex space-x-60 ">
                 <div className="mt-16 basis-1/2 md:mt-0">
                     <img alt="logo" src={Logo} />
-                    
+
                     <p>© MarcosGomes All Rights Reserved.</p>
                 </div>
-                <div className="mt-16 basis-1/4 md:mt-0">
-                    <h4 className="font-bold">Links</h4>
-                    <p className="my-5">Massa orci senectus</p>
-                    <p className="my-5">Et gravida id et etiam</p>
-                    <p>Ullamcorper vivamus</p>
-                </div>
-                <div className="mt-16 basis-1/4 md:mt-0">
-                    <h4 className="font-bold">Contact Us</h4>
-                    <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-                    <p>(333)425-6825</p>
+                <div className="mt-16 basis-1/4 md:mt-0 ">
+                    <h4 className="font-bold">Contatos</h4>
+                    <p>(65)98403-6755</p>
+                    <div className="flex">
+                        <a href="https://www.instagram.com/solar_construcoes23/" target="_blank" rel="noreferrer " className="flex">
+                            <img src={Instagram} alt="Instagram" className="w-16" />
+                        </a>
+                        <a href="https://google.com" target="_blank" rel="noreferrer " className="flex">
+                            <img src={WhatsApp} alt="WhatsApp" className="w-20" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </footer>
